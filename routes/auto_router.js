1d42104985bbcdebd477ffbf57ef1f9f3cb57051
Nathaly Zavala <24iveth.ramirez@gmail.com>
2021-02-24 17:53:16 -0300

    Se actualizo el router

--- routes/auto_router.js
+++ routes/auto_router.js
@@ -2,25 +2,25 @@ const {Router} = require('express');
 const autoService = require('../services/auto_service');
 const router = Router();
 
-router.get('/select', (req, res) => {
+router.get('/autos', (req, res) => {
     autoService.getAutos().then(data => {
         res.status(data.statusCode).json(data)
     });
 });
 
-router.post('/save', (req, res) => {
+router.post('/autos', (req, res) => {
     autoService.saveAutos(req.body).then(data => {
         res.status(data.statusCode).json(data)
     });
 });
 
-router.delete('/delete/:idauto', (req, res) => {
+router.delete('/autos/:idauto', (req, res) => {
     autoService.deleteAutos(req.params.idauto).then(data => {
         res.status(data.statusCode).json(data)
     });
 });
 
-router.patch('/update', (req, res) => {
+router.patch('/autos', (req, res) => {
     autoService.updateAutos(req.body).then(data => {
         res.status(data.statusCode).json(data)
     });
